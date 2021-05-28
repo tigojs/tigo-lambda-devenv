02dc60fb67144a98e959e22c09e5cd081f4a3f6a
BackRunner <dev@backrunner.top>
2021-05-28 17:28:12 +0800

    fix: packing modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,24 @@
 const { babel } = require('@rollup/plugin-babel');
-const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
 const fs = require('fs');
+
+const PREFIX_WHITELIST = ['@tigojs/lambda-', '@tigojs/api-'];
 
 const pkg = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf-8' }));
 const devrc = JSON.parse(fs.readFileSync('./.tigodev.json', { encoding: 'utf-8' }));
 
-// regard all packages as external by default
-const external = Object.keys(pkg.dependencies || {});
+// only module in require allow list are external package
+const external = [];
+const configuredExternal = devrc?.rollup?.external;
+if (configuredExternal && Array.isArray(configuredExternal)) {
+  external = external.concat();
+}
+Object.keys(pkg.dependencies).forEach((dependency) => {
+  if (PREFIX_WHITELIST.includes(dependency)) {
+    external.push(dependency);
+  }
+});
 
 const extensions = ['.js'];
 
@@ -22,10 +34,8 @@ const options = {
   },
   external,
   plugins: [
-    nodeResolve({
-      extensions,
-      modulesOnly: true,
-    }),
+    nodeResolve(),
+    commonjs(),
     babel({
       exclude: ['node_modules/**'],
       babelHelpers: 'bundled',
